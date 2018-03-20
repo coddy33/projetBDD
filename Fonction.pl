064a1b49aab24db278de00c5ea4587ef3c94b4ae
Martial Bodet <mbodet911e@emi.u-bordeaux1.fr>
2018-03-20 21:44:44 +0100

    Il y a les trois premiere fonction, On baise des putes, snif de la coc' et nique des mere bro !!! :D

--- Fonction.pl
+++ Fonction.pl
@@ -22,21 +22,42 @@
 
 #Fonction qui affiche le nombre de gerant.
 
+# use strict;
+# use warnings;
+# use DBI;
+#
+# my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
+#
+# my $requete = "SELECT COUNT(DISTINCT gerant)  FROM tablehotel ";
+# my $prep = $dbh->prepare($requete);
+#     #or die "Impossible de préparer la requête : ".$dbh->errstr;
+#
+# $prep->execute;
+# my @tab;
+#     #or die 'Impossible d\'exécuter la requête : '.$prep->errstr;
+# while (my($gerant) = $prep->fetchrow_array ) {
+#     print "$gerant\n";
+#     #  print "Le gerant de l'hotel $hotel est monsieur $gerant\n";
+#
+#     }
+
+
+#Afficher le nom de personne qui gerent au moins deux hotel.
 use strict;
 use warnings;
 use DBI;
 
 my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
 
-my $requete = "SELECT COUNT(DISTINCT gerant)  FROM tablehotel ";
+my $requete = "SELECT gerant  FROM tablehotel GROUP BY gerant HAVING COUNT(*) >=2";
 my $prep = $dbh->prepare($requete);
     #or die "Impossible de préparer la requête : ".$dbh->errstr;
-
 $prep->execute;
-my @tab;
+
     #or die 'Impossible d\'exécuter la requête : '.$prep->errstr;
 while (my($gerant) = $prep->fetchrow_array ) {
     print "$gerant\n";
+
     #  print "Le gerant de l'hotel $hotel est monsieur $gerant\n";
 
     }
