1d32c6bbdb0e48a5467cdafc238e9db3d1ef6613
Frederic Jung <fjung@emi.u-bordeaux1.fr>
2018-03-22 16:06:17 +0100

    update menu + factorsation

--- projet.pl
+++ projet.pl
@@ -6,132 +6,143 @@ use DBI;
 
 my $file = "Hotels1.csv";
 
-
-
-sub initialisation{
 my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
 
-$dbh -> do ("drop table if exists initTable cascade");
-$dbh -> do ("drop table if exists TableHotel cascade");
-$dbh -> do ("drop table if exists Reservation cascade");
-$dbh -> do ("drop table if exists client cascade");
-$dbh -> do ("drop table if exists chambre cascade");
+sub initialisation{
+        
+        $dbh -> do ("drop table if exists initTable cascade");
+        $dbh -> do ("drop table if exists TableHotel cascade");
+        $dbh -> do ("drop table if exists Reservation cascade");
+        $dbh -> do ("drop table if exists client cascade");
+        $dbh -> do ("drop table if exists chambre cascade");
 
-my $sth = $dbh->prepare("
+        my $sth = $dbh->prepare("
 
-create table initTable(
-Hotel text,
-Gerant text,
-Etoiles integer,
-NumChambre integer,
-TypeCouchage text,
-PrixBasseSaison integer,
-PrixHauteSaison integer,
-NumResa integer,
-DebutResa text,
-FinResa text,
-NomClient text,
-PhoneClient text);
+        create table initTable(
+        Hotel text,
+        Gerant text,
+        Etoiles integer,
+        NumChambre integer,
+        TypeCouchage text,
+        PrixBasseSaison integer,
+        PrixHauteSaison integer,
+        NumResa integer,
+        DebutResa text,
+        FinResa text,
+        NomClient text,
+        PhoneClient text);
 
-create table TableHotel(
-Hotel text,
-Gerant text,
-Etoiles integer);
+        create table TableHotel(
+        Hotel text,
+        Gerant text,
+        Etoiles integer);
 
-CREATE TABLE Reservation(
-NumResa integer,
-DebutResa text,
-FinResa text,
-NumChambre integer,
-Hotel text,
-NomClient text);
-
-
-CREATE TABLE Client(
-NomClient text,
-PhoneClient text);
+        CREATE TABLE Reservation(
+        NumResa integer,
+        DebutResa text,
+        FinResa text,
+        NumChambre integer,
+        Hotel text,
+        NomClient text);
 
 
-CREATE TABLE Chambre(
-NumChambre text,
-Hotel text,
-Typecouchage text,
-PrixBasseSaison integer,
-PrixHauteSaison integer);
-");
+        CREATE TABLE Client(
+        NomClient text,
+        PhoneClient text);
+
+
+        CREATE TABLE Chambre(
+        NumChambre text,
+        Hotel text,
+        Typecouchage text,
+        PrixBasseSaison integer,
+        PrixHauteSaison integer);
+        ");
 
 
 
-$sth->execute();
+        $sth->execute();
 
-my $x = 1;
-open(HOTEL,$file) or die("$file inexistant \n");
-my $remplissage = $dbh -> prepare("insert into initTable values (?,?,?,?,?,?,?,?,?,?,?,?)");
-while (<HOTEL>){
-    if ($x != 1){
-    chomp($_);
-    my @fields = split(',',$_);   
-    $remplissage -> execute($fields[0],$fields[1],int($fields[2]),int($fields[3]),$fields[4],int($fields[5]),int($fields[6]),int($fields[7]),$fields[8],$fields[9],$fields[10],$fields[11]);
-}
-    $x = $x -1;
-}
+        my $x = 1;
+        open(HOTEL,$file) or die("$file inexistant \n");
+        my $remplissage = $dbh -> prepare("insert into initTable values (?,?,?,?,?,?,?,?,?,?,?,?)");
+        while (<HOTEL>){
+            if ($x != 1){
+            chomp($_);
+            my @fields = split(',',$_);   
+            $remplissage -> execute($fields[0],$fields[1],int($fields[2]),int($fields[3]),$fields[4],int($fields[5]),int($fields[6]),int($fields[7]),$fields[8],$fields[9],$fields[10],$fields[11]);
+        }
+            $x = $x -1;
+        }
 
-my $initHotel = $dbh->prepare("
+        my $initHotel = $dbh->prepare("
 
-insert into TableHotel(
-select hotel, gerant, etoiles
-From inittable );
-");
+        insert into TableHotel(
+        select hotel, gerant, etoiles
+        From inittable );
+        ");
 
-my $initResa = $dbh->prepare("
-INSERT INTO Reservation(
-SELECT NumResa, DebutResa, FinResa, NumChambre, Hotel, NomClient
-FROM InitTable);
-");
+        my $initResa = $dbh->prepare("
+        INSERT INTO Reservation(
+        SELECT NumResa, DebutResa, FinResa, NumChambre, Hotel, NomClient
+        FROM InitTable);
+        ");
 
-my $initClient = $dbh->prepare("
-INSERT INTO Client(
-SELECT NomClient, PhoneClient
-FROM InitTable);
-");
+        my $initClient = $dbh->prepare("
+        INSERT INTO Client(
+        SELECT NomClient, PhoneClient
+        FROM InitTable);
+        ");
 
-my $initChambre = $dbh->prepare("
-INSERT INTO Chambre(
-SELECT NumChambre, Hotel, TypeCouchage, PrixBasseSaison, PrixHauteSaison
-FROM InitTable);
-");
+        my $initChambre = $dbh->prepare("
+        INSERT INTO Chambre(
+        SELECT NumChambre, Hotel, TypeCouchage, PrixBasseSaison, PrixHauteSaison
+        FROM InitTable);
+        ");
 
-$initHotel->execute();
-$initResa->execute();
-$initClient->execute();
-$initChambre->execute();
+        $initHotel->execute();
+        $initResa->execute();
+        $initClient->execute();
+        $initChambre->execute();
 
-close(HOTEL);
-$dbh -> disconnect();
+        close(HOTEL);
 
 } #Fin de la fonction initialisation
 
 
 # ===================INTEGRATION===================
 
+# sub test{
+#     # print "coucou";
+#     my $prep = $dbh->prepare(@_);
+#     $prep->execute;
+#         #or die 'Impossible d\'eécuter la requête : '.$prep->errstr;
+#     # return $prep;
+#     my @col1, my @col2, my @col3; 
+#     while (my($cell1, $cell2) = @_->fetchrow_array ) {
+#     push(@col1,$cell1);
+#     push(@col2,$cell2);
+#     }
+# }
 
-sub afficher_gerant{
-my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
-my $requete = "SELECT gerant,hotel  FROM tablehotel";
-my $prep = $dbh->prepare($requete);
-$prep->execute;
-    #or die 'Impossible d\'exécuter la requête : '.$prep->errstr;
-while (my($gerant,$hotel) = $prep->fetchrow_array ) {
-
-      print "$hotel -> $gerant\n";
-    #  print "Le gerant de l'hotel $hotel est monsieur $gerant\n";
+sub test{
+    my $prep = $dbh->prepare(@_);
+    $prep->execute;
+    while (my $row = $prep->fetchrow_hashref) {
+        my @fig = sort(keys(%$row));
+        foreach my $fname (@fig) {
+        print "$row->{$fname} ";
+        }
+    print "\n";
     }
 }
+
+# =================================================
+
 
 # ===================MISE A JOUR===================
 
 sub ajouter_chambre{
-    my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
     print "Pour quel hotel voulez vous ajouter une chambre ? \n";
     my $requete = "SELECT hotel FROM Chambre GROUP BY hotel";
     my $prep = $dbh->prepare($requete);
@@ -170,7 +181,8 @@ sub menu{
     print "=========================MENU========================= \n";
     print "\t [1] Interrogation \n";
     print "\t [2] Mise à jour \n";
-    print "\t [2] Statistiques \n" 
+    print "\t [3] Statistiques \n";
+    print "\t [0] Quitter \n";
 }
 
 
@@ -181,6 +193,27 @@ sub menu_interrogation{
     print "\t [3] Afficher les personnes qui gèrent au moins deux hôtels \n";
     print "\t [4] Afficher les hôtels où il y a au moins une chambre de libre \n";    
 }
+sub interrogation{
+    my $rep = <>;
+    if ($rep == 1){
+        print "Les gérants se nomment : \n";        
+        test("SELECT gerant,hotel  FROM tablehotel");
+    }
+    if ($rep == 2){
+        print "Le nombre de gérants est de : \n";
+        test("SELECT COUNT(DISTINCT gerant)  FROM tablehotel");
+    } if ($rep == 3){
+        print "Les gérants qui gérent au moins deux hotels sont : \n";
+        test("SELECT gerant  FROM tablehotel GROUP BY gerant HAVING COUNT(*) >=2");
+    }if ($rep == 4){
+        print "Les hotels qui ont au moins une chambre de libre sont : \n";
+        test("*");
+    }
+}
+
+
+SELECT hotel FROM  GROUP BY NomImmeuble HAVING   COUNT(*) > 3
+
 
 sub menu_maj{
     print "=========================MENU========================= \n";
@@ -189,12 +222,18 @@ sub menu_maj{
     print "\t [3] Annuler une réservation\n";    
     print "\t [4] Ajouter une réservation\n";        
 }
+sub maj{
+    my $rep = <>;
+    if ($rep == 1){
+        ajouter_chambre();
+    }
+}
 
 sub menu_stats{
     print "=========================MENU========================= \n";
     print "\t [1] Afficher le taux d'occupation d'un hôtel (7 derniers jours)\n";    
-    print "\t [1] Afficher le taux d'occupation de tous les hôtels (7 derniers jours)\n";    
-    print "\t [1] Afficher les ou les hôtels qui ont le plus grand taux d'occupation (7 derniers jours)\n";        
+    print "\t [2] Afficher le taux d'occupation de tous les hôtels (7 derniers jours)\n";    
+    print "\t [3] Afficher les ou les hôtels qui ont le plus grand taux d'occupation (7 derniers jours)\n";        
 }
 
 my $boucle = 1; 
@@ -203,10 +242,18 @@ while ($boucle == 1){
     menu;
     my $rep = <>;
     if ($rep == 1){
-        afficher_gerant();
+        menu_interrogation();
+        interrogation();
     }
     if ($rep == 2){
-        ajouter_chambre();
+        menu_maj();
+    }
+    if ($rep == 3){
+        menu_stats();
+    }
+    if ($rep == 0){
+        $dbh -> disconnect();        
+        exit;
     }
 }
 
