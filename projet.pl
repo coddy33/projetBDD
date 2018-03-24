8b1282db8981092500d406dadb297f783768c0e1
s3bc40 <s.goncalvesclaro@gmail.com>
2018-03-24 18:09:49 +0100

    taux occupation function

--- projet.pl
+++ projet.pl
@@ -179,6 +179,34 @@ sub modifier_gerant{
 
 }
 
+# ================== STATISTIQUES ===========
+sub taux_occupation {
+# Date : today and earlier week
+my($h) = @_;
+my $today = DateTime->new ( day => 26,
+                            month =>02,
+                            year =>2018
+                            );
+my $weekEarly = $today->clone->subtract( weeks => 1);
+
+# Main 
+my $requete1 = qq(SELECT COUNT(*)  FROM reservation WHERE hotel = '$h'); # Total number of chambers 
+my $requete2 = qq(SELECT COUNT(*)  FROM reservation WHERE hotel = '$h' AND to_date(debutresa,'DD/MM/YYYY') <='$today' AND to_date(finresa,'DD/MM/YYYY') >= '$weekEarly');
+my $prep1 = $dbh->prepare($requete1);
+my $prep2 = $dbh->prepare($requete2);
+$prep1->execute;
+    #or die 'Impossible d\'exécuter la requête : '.$prep->errstr;
+while (my($lineCount) = $prep1->fetchrow_array ) {
+    $prep2->execute;
+        #or die 'Impossible d\'exécuter la requête : '.$prep->errstr;
+    while (my($dateCount) = $prep2->fetchrow_array ) {
+        my $taux = ($dateCount / $lineCount)*100;
+        print "$h -> $taux%\n";
+        #  print "Le gerant de l'hotel $hotel est monsieur $gerant\n";
+        }
+    }
+}
+
 
 # ===================MENU===================
 
