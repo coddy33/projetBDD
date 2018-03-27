8ee497a0147f170df142a13b7fa4ea63a45e8fbf
Martial Bodet <mbodet911e@emi.u-bordeaux1.fr>
2018-03-27 09:00:43 +0200

    BITE

--- projet.pl
+++ projet.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use DBI;
 
-
 my $file = "Hotels1.csv";
 
 
@@ -12,6 +11,10 @@ sub initialisation{
 my $dbh = DBI -> connect("DBI:Pg:dbname=fjung;host=dbserver","fjung","idiot21",{'RaiseError' => 1});
 
 $dbh -> do ("drop table if exists initTable cascade");
+$dbh -> do ("drop table if exists TableHotel cascade");
+$dbh -> do ("drop table if exists Reservation cascade");
+$dbh -> do ("drop table if exists client cascade");
+$dbh -> do ("drop table if exists chambre cascade");
 
 my $sth = $dbh->prepare("
 
@@ -27,8 +30,37 @@ NumResa integer,
 DebutResa text,
 FinResa text,
 NomClient text,
-PhoneClient text);     
+PhoneClient text);
+
+create table TableHotel(
+Hotel text,
+Gerant text,
+Etoiles integer);
+
+CREATE TABLE Reservation(
+NumResa integer,
+DebutResa text,
+FinResa text,
+NumChambre integer,
+Hotel text,
+NomClient text);
+
+
+CREATE TABLE Client(
+NomClient text,
+PhoneClient text);
+
+
+CREATE TABLE Chambre(
+NumChambre text,
+Hotel text,
+Typecouchage text,
+PrixBasseSaison integer,
+PrixHauteSaison integer);
 ");
+
+
+
 $sth->execute();
 
 my $x = 1;
@@ -37,19 +69,62 @@ my $remplissage = $dbh -> prepare("insert into initTable values (?,?,?,?,?,?,?,?
 while (<HOTEL>){
     if ($x != 1){
     chomp($_);
-    my @fields = split(',',$_);   
+    my @fields = split(',',$_);
     $remplissage -> execute($fields[0],$fields[1],int($fields[2]),int($fields[3]),$fields[4],int($fields[5]),int($fields[6]),int($fields[7]),$fields[8],$fields[9],$fields[10],$fields[11]);
 }
     $x = $x -1;
 }
-close(HOTEL);
 
+my $initHotel = $dbh->prepare("
+
+insert into TableHotel(
+select hotel, gerant, etoiles
+From inittable );
+");
+
+my $initResa = $dbh->prepare("
+INSERT INTO Reservation(
+SELECT NumResa, DebutResa, FinResa, NumChambre, Hotel, NomClient
+FROM InitTable);
+");
+
+my $initClient = $dbh->prepare("
+INSERT INTO Client(
+SELECT NomClient, PhoneClient
+FROM InitTable);
+");
+
+my $initChambre = $dbh->prepare("
+INSERT INTO Chambre(
+SELECT NumChambre, Hotel, TypeCouchage, PrixBasseSaison, PrixHauteSaison
+FROM InitTable);
+");
+
+$initHotel->execute();
+$initResa->execute();
+$initClient->execute();
+$initChambre->execute();
+
+close(HOTEL);
 $dbh -> disconnect();
-}
+
+} #Fin de la fonction initialisation
+
+
+# ===================MENU===================
 
 sub menu{
     print "=========================MENU========================= \n";
     print "[1] Afficher la Table d'initialisation \n";
 }
 
-menu();
+
+my $boucle = 1;
+initialisation();
+while ($boucle == 1){
+    menu;
+    my $rep = <>;
+    if ($rep == 1){
+        print "FILS DE PUTE \n";
+    }
+}
